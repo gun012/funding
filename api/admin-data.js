5aa78b8f7a2efea31ce06e6656eeddecc9dc5974
gun012 <jg03200324@gmail.com>
2026-05-19 02:13:56 +0900

    Update admin-data.js

--- api/admin-data.js
+++ api/admin-data.js
@@ -1,1 +1,23 @@
+export default async function handler(req, res) {
+  try {
+    const WEB_APP_URL = process.env.GOOGLE_SCRIPT_URL;
 
+    if (!WEB_APP_URL) {
+      return res.status(500).json({
+        ok: false,
+        error: "GOOGLE_SCRIPT_URL 환경변수가 없습니다.",
+      });
+    }
+
+    const response = await fetch(WEB_APP_URL + "?action=dashboard");
+    const data = await response.json();
+
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.status(500).json({
+      ok: false,
+      error: "admin data api error",
+      message: error.message,
+    });
+  }
+}
