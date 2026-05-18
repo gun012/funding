3eae85b10a25d9977188d1c067028fe12acb30c3
gun012 <jg03200324@gmail.com>
2026-05-19 01:42:12 +0900

    Update dashboard.js

--- api/dashboard.js
+++ api/dashboard.js
@@ -10,7 +10,6 @@ export default async function handler(req, res) {
     }
 
     const response = await fetch(WEB_APP_URL + "?action=dashboard");
-
     const data = await response.json();
 
     return res.status(200).json(data);
