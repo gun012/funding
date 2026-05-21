1e725ee32062d4da0fceb83f4218e4c9906ad0c5
gun012 <jg03200324@gmail.com>
2026-05-21 20:01:55 +0900

    Update notices.js

--- api/notices.js
+++ api/notices.js
@@ -1,30 +1,25 @@
 export default async function handler(req, res) {
   try {
-
-    const url =
-      "https://www.bizinfo.go.kr/uss/rss/bizinfoApi.do?crtfcKey=&dataType=xml&searchCnt=30";
+    const url = "https://mss.go.kr/rss/smba/board/310.do";
 
     const response = await fetch(url, {
       headers: {
         "User-Agent": "Mozilla/5.0",
+        "Accept": "application/rss+xml,text/xml,*/*",
       },
     });
 
     const text = await response.text();
 
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Content-Type",
-      "text/xml; charset=utf-8"
-    );
+    res.setHeader("Content-Type", "text/xml; charset=utf-8");
 
     return res.status(200).send(text);
 
   } catch (error) {
-
     return res.status(500).json({
       ok: false,
-      error: "notice proxy error",
+      error: "mss notice proxy error",
       message: error.message,
     });
   }
